4fc98002350fdcf2e7107d04d7b49edf86c262e4
Calenzo <calenzodevs@gmail.com>
2022-06-18 23:06:22 -0300

    feat: Preparado os serviços para implementar o firebase

---
+++ src/infrastructure/services/cards/index.js
@@ -0,0 +1,7 @@
+export const createCard = async ({ item }) => {};
+
+export const readCards = async () => {};
+
+export const updateCard = async ({ item }) => {};
+
+export const deleteCard = async ({ item }) => {};
---
+++ src/infrastructure/services/firebase/index.js
@@ -0,0 +1,7 @@
+export const getService = async ({ collection }) => {};
+
+export const putService = async ({ collection, document, data = {} }) => {};
+
+export const postService = async ({ collection, data = {} }) => {};
+
+export const deleteService = async ({ collection, document }) => {};
